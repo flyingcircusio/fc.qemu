c1200e1f397af90403aeb5ab840268429f22a08f
Christian Theune <ct@gocept.com>
2014-09-24 14:04:57 +0200

    Preparing release 0.4.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fc.qemu',
-    version='0.4.1dev0',
+    version='0.4.1',
     author='Christian Kauhaus, Christian Theune',
     author_email='mail@gocept.com',
     url='http://bitbucket.org/flyingcircus/fc.qemu',
