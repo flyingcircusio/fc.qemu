a532c187046c6a5980b15451dda89adcb605319a
Christian Kauhaus <kc@flyingcircus.io>
2015-01-12 15:22:52 +0100

    Improve test coverage for incoming migration and file rewrite
    Re #13229

---
+++ src/fc/qemu/tests/util_test.py
@@ -0,0 +1,37 @@
+from ..util import rewrite
+import pytest
+import os
+import tempfile
+
+
+@pytest.yield_fixture
+def tmp():
+    tf = tempfile.NamedTemporaryFile(mode='w', delete=False)
+    yield tf
+    try:
+        os.unlink(tf.name)
+    except OSError:  # pragma: nocover
+        pass
+
+
+def test_rewrite(tmp):
+    tmp.write('old content\n')
+    tmp.flush()
+    old_ino = os.fstat(tmp.fileno()).st_ino
+    with rewrite(tmp.name) as f:
+        f.write('new content\n')
+        f.flush()
+    with open(tmp.name) as f:
+        assert 'new content\n' == f.read()
+    assert old_ino != os.stat(tmp.name).st_ino
+
+
+def test_rewrite_ignore_deleted(tmp):
+    tmp.write('old content\n')
+    tmp.flush()
+    old_ino = os.fstat(tmp.fileno()).st_ino
+    with rewrite(tmp.name) as f:
+        os.unlink(f.name)
+    with open(tmp.name) as f:
+        assert 'old content\n' == f.read()
+    assert old_ino == os.stat(tmp.name).st_ino
