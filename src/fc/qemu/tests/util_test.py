08d42c39f4eea628e5827900c6cd94a071d11971
Christian Kauhaus <kc@flyingcircus.io>
2015-01-14 15:39:11 +0100

    Add tests for rewrite
    Re #13229

--- src/fc/qemu/tests/util_test.py
+++ src/fc/qemu/tests/util_test.py
@@ -1,6 +1,6 @@
 from ..util import rewrite
-import pytest
 import os
+import pytest
 import tempfile
 
 
@@ -35,3 +35,11 @@ def test_rewrite_ignore_deleted(tmp):
     with open(tmp.name) as f:
         assert 'old content\n' == f.read()
     assert old_ino == os.stat(tmp.name).st_ino
+
+
+def test_rewrite_should_create_nonexisting_file(tmp):
+    os.unlink(tmp.name)
+    with rewrite(tmp.name) as f:
+        f.write('new file\n')
+    with open(tmp.name) as f:
+        assert 'new file\n' == f.read()
--- src/fc/qemu/util.py
+++ src/fc/qemu/util.py
@@ -10,13 +10,14 @@ def rewrite(filename):
     with tempfile.NamedTemporaryFile(
             dir=os.path.dirname(filename), prefix=os.path.basename(filename),
             delete=False) as tf:
-        os.chmod(tf.name, os.stat(filename).st_mode & 0o7777)
+        if os.path.exists(filename):
+            os.chmod(tf.name, os.stat(filename).st_mode & 0o7777)
         yield tf
+        if not os.path.exists(tf.name):
+            # Allow our clients to remove the file in case it doesn't want it
+            # to be put in place actually but also doesn't want to error out.
+            return
         tf.flush()
         os.fdatasync(tf)
         filename_tmp = tf.name
-    if not os.path.exists(filename_tmp):
-        # Allow our clients to remove the file in case it doesn't want it to be
-        # put in place actually but also doesn't want to error out.
-        return
     os.rename(filename_tmp, filename)
