e22cecf7509a245ac692d531d209db91c0ee6d45
Christian Kauhaus <kc@flyingcircus.io>
2015-01-14 15:39:58 +0100

    Improve vagrant setup
    If this are still old boxes around, kill all vagrant processes and remove
    .vagrant/machines/*.
    
    Re #13229

--- bootstrap-ceph.sh
+++ bootstrap-ceph.sh
@@ -6,9 +6,6 @@ apt-get install -y ceph-deploy librbd1 ceph python-dev
 
 ceph-deploy install host1 host2
 ceph-deploy new host1 host2
-cat >> ceph.conf <<EOF
-osd pool default size = 1
-EOF
 ceph-deploy mon create-initial host1 host2
 ceph-deploy osd create host1:/var/local/osd0 host2:/var/local/osd1
 ceph-deploy osd activate host1:/var/local/osd0 host2:/var/local/osd1
