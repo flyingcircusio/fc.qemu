3e805eed269ae5e1790c142ac5ba626f22d1a3ca
Christian Kauhaus <kc@flyingcircus.io>
2016-03-08 16:36:26 +0100

    Improve test coverage and fix edge cases
    Re #19079

--- src/fc/qemu/hazmat/tests/test_ceph.py
+++ src/fc/qemu/hazmat/tests/test_ceph.py
@@ -6,7 +6,7 @@ import rbd
 def ceph_with_volumes(ceph_inst):
     for vol in ceph_inst.volumes:
         vol.ensure_presence()
-        vol.lock()
+    ceph_inst.lock()
     yield ceph_inst
     for vol in ceph_inst.volumes:
         vol.unlock(force=True)
@@ -30,3 +30,9 @@ def test_ceph_stop_remove_only_own_locks(ceph_with_volumes):
     assert ceph_with_volumes.root.lock_status()
     assert ceph_with_volumes.swap.lock_status() is None
     assert ceph_with_volumes.tmp.lock_status() is None
+
+
+def test_is_unlocked(ceph_with_volumes):
+    assert ceph_with_volumes.is_unlocked() is False
+    ceph_with_volumes.unlock()
+    assert ceph_with_volumes.is_unlocked() is True
