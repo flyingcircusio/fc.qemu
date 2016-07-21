dcbbb17f1b7f2dd8c642c2119e2328084ae97136
Christian Kauhaus <kc@flyingcircus.io>
2016-07-21 12:20:40 +0200

    Preparing release 0.7.22

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('HACKING.txt') as f:
 
 setup(
     name='fc.qemu',
-    version='0.7.22.dev0',
+    version='0.7.22',
     author='Christian Kauhaus, Christian Theune',
     author_email='mail@flyingcircus.io',
     url='http://bitbucket.org/flyingcircus/fc.qemu',
