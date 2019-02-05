32afc03d0b3ce8e52e4a73c7543514436643dd59
Christian Theune <ct@flyingcircus.io>
2019-02-05 20:15:21 +0100

    Preparing release 1.0.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('HACKING.txt') as f:
 
 setup(
     name='fc.qemu',
-    version='1.0.4.dev0',
+    version='1.0.4',
     author='Christian Kauhaus, Christian Theune',
     author_email='mail@flyingcircus.io',
     url='http://bitbucket.org/flyingcircus/fc.qemu',
