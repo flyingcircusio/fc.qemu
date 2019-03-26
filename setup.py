d7c23e4e2d5c3904befa6e32c7c2a77188da8001
Christian Kauhaus <kc@flyingcircus.io>
2019-03-26 13:56:32 +0100

    Back to development: 1.0.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('HACKING.txt') as f:
 
 setup(
     name='fc.qemu',
-    version='1.0.6',
+    version='1.0.7.dev0',
     author='Christian Kauhaus, Christian Theune',
     author_email='mail@flyingcircus.io',
     url='http://bitbucket.org/flyingcircus/fc.qemu',
