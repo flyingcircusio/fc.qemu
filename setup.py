8e4159a246f818fb13d1bf28efda8e2c712432bf
Oliver Schmidt <os@flyingcircus.io>
2024-01-17 14:09:01 +0100

    back to development: 1.4.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("HACKING.txt") as f:
 
 setup(
     name="fc.qemu",
-    version="1.4.3",
+    version="1.4.4.dev0",
     author="Christian Kauhaus, Christian Theune",
     author_email="mail@flyingcircus.io",
     url="http://github.com/flyingcircusio/fc.qemu",
