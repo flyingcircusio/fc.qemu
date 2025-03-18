2718c1d414728f66b68868d0d63198fc38a45dc5
Christian Theune <ct@flyingcircus.io>
2025-03-18 13:00:07 +0100

    improve multi-vm setup, avoid fixed ip addresses

--- batou/components/nixos/base.nix
+++ batou/components/nixos/base.nix
@@ -12,13 +12,13 @@ in
   flyingcircus.encServices = [
     {
       address = "host1";
-      ips = [ "10.12.0.6" ];
+      ips = [ "{{component.host1_addr.listen.host}}" ];
       location = "test";
       service = "consul_server-server";
     }
     {
       address = "host1";
-      ips = [ "10.12.0.6" ];
+      ips = [ "{{component.host1_addr.listen.host}}" ];
       location = "test";
       service = "ceph_mon-mon";
     }
@@ -28,8 +28,8 @@ in
   flyingcircus.agent.enable = lib.mkForce false;
 
   networking.extraHosts = ''
-    10.12.0.6 host1.srv.test.gocept.net host1
-    10.12.0.10 host2.srv.test.gocept.net host2
+    {{component.host1_addr.listen.host}} host1.srv.test.gocept.net host1
+    {{component.host2_addr.listen.host}} host2.srv.test.gocept.net host2
   '';
 
   flyingcircus.static.ceph.fsids.test.test = "d118a9a4-8be5-4703-84c1-87eada2e6b60";
