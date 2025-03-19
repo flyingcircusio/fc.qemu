1aa178bb13101951ed34d1623b93441e49222bec
Molly Miller <mm@flyingcircus.io>
2025-03-19 10:35:44 +0100

    Manage host routes for guest interfaces in layer 3 VRFs
    In order for guests interfaces which are connected to layer 3 VRFs to
    be reachable, we need to manage routes installed in the VRF routing
    table on the host.
    
    This adds an extra step to the ensure process which reconciles the
    state of the VRF routing table to ensure the correct routes are
    assigned for guest tap interfaces. This adds a dependency on iproute2
    for management of the routes, and we also provide an rt_protos file
    with an fc-qemu route protocol in order make the management of routes
    by fc.qemu visible in the routing table.

--- src/fc/qemu/agent.py
+++ src/fc/qemu/agent.py
@@ -5,6 +5,7 @@ import distutils.spawn
 import fcntl
 import json
 import math
+import operator
 import os
 import shutil
 import socket
@@ -13,6 +14,7 @@ import sys
 import time
 import typing
 from codecs import decode
+from ipaddress import ip_interface
 from multiprocessing.pool import ThreadPool
 from pathlib import Path
 from typing import Optional
@@ -251,6 +253,20 @@ def tmp_size(disk):
     """Returns the tmp partition size in bytes."""
     tmp_gib = max(5, math.sqrt(disk))
     return int(tmp_gib) * GiB
+
+
+def iproute2_json(*args):
+    cmd = ["ip", "-j"]
+    cmd.extend(args)
+    data = subprocess.check_output(cmd)
+    data = data.decode("utf-8", errors="replace")
+    return json.loads(data if data else "null")
+
+
+# The iteration order of items in a set is non-deterministic which is
+# a problem the integration tests
+def sorted_ipset(items):
+    return sorted(items, key=operator.attrgetter("version", "ip"))
 
 
 class Agent(object):
@@ -1093,6 +1109,7 @@ class Agent(object):
         self.consul_register()
         self.ensure_online_disk_size()
         self.ensure_online_disk_throttle()
+        self.ensure_online_host_routes()
         self.ensure_watchdog()
         self.ceph.ensure()
         # Be aggressive/opportunistic about re-acquiring locks in case
@@ -1213,6 +1230,90 @@ class Agent(object):
                     action="none",
                 )
 
+    def ensure_online_host_routes(self):
+        """Ensure that host VRF routes are configured for this VM."""
+        for net, net_config in sorted(self.cfg["interfaces"].items()):
+            if "routed" not in net_config or not net_config["routed"]:
+                continue
+            ifname = "t{}{}".format(net, self.cfg["id"])
+            vrfname = "vrf{}".format(net)
+            target_routes = {
+                ip_interface(addr)
+                for addrs in net_config["networks"].values()
+                for addr in addrs
+            }
+            current_routes = {}
+
+            self.log.info(
+                "ensure-routes",
+                iface=ifname,
+                vrf=vrfname,
+                action="start",
+            )
+
+            try:
+                current_v4 = iproute2_json(
+                    "-4", "route", "show", "vrf", vrfname, "dev", ifname
+                )
+                current_v6 = iproute2_json(
+                    "-6", "route", "show", "vrf", vrfname, "dev", ifname
+                )
+
+                current_routes = {
+                    ip_interface(x["dst"])
+                    for x in (current_v4 + current_v6)
+                    # ignore routes managed by the kernel
+                    if "protocol" not in x or x["protocol"] != "kernel"
+                }
+
+                add = target_routes - current_routes
+                remove = current_routes - target_routes
+
+                if add or remove:
+                    self.log.info(
+                        "ensure-routes",
+                        iface=ifname,
+                        vrf=vrfname,
+                        current_routes=[
+                            x.with_prefixlen
+                            for x in sorted_ipset(current_routes)
+                        ],
+                        target_routes=[
+                            x.with_prefixlen
+                            for x in sorted_ipset(target_routes)
+                        ],
+                        action="reconciling",
+                    )
+
+                for x in add:
+                    iproute2_json(
+                        # fmt: off
+                        f"-{x.version}", "route", "add", f"{x}",
+                        "dev", ifname, "vrf", vrfname, "proto", "fc-qemu"
+                        # fmt: on
+                    )
+                for x in remove:
+                    iproute2_json(
+                        # fmt: off
+                        f"-{x.version}", "route", "del", f"{x}",
+                        "dev", ifname, "vrf", vrfname
+                        # fmt: on
+                    )
+
+                self.log.info(
+                    "ensure-routes",
+                    iface=ifname,
+                    vrf=vrfname,
+                    action="finished",
+                )
+            except subprocess.CalledProcessError:
+                self.log.exception(
+                    "ensure-routes-failed",
+                    iface=ifname,
+                    vrf=vrfname,
+                    exc_info=True,
+                )
+
     def ensure_watchdog(self, action="none"):
         """Ensure watchdog settings."""
         self.log.info("ensure-watchdog", action=action)
@@ -1262,6 +1363,7 @@ class Agent(object):
         self.qemu.start()
         self.consul_register()
         self.ensure_online_disk_throttle()
+        self.ensure_online_host_routes()
         self.ensure_watchdog()
         # We exit here without releasing the ceph lock in error cases
         # because the start may have failed because of an already running
